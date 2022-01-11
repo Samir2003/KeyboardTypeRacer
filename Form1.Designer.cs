99eaa350d9b16e9667e9e424e82b38f7c8195e50
Samir Ali <353385354@educ.dpcdsb.org>
2022-01-11 17:48:11 -0500

    done

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace KeyboardTypeRacer
+{
+    partial class KeyboardTyper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.timerResult = new System.Windows.Forms.Timer(this.components);
+            this.btnStore = new System.Windows.Forms.Button();
+            this.btnFruits = new System.Windows.Forms.Button();
+            this.btnVegetables = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtInput.ForeColor = System.Drawing.Color.White;
+            this.txtInput.Location = new System.Drawing.Point(331, 204);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 30);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(139, 22);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(589, 40);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Welcome to Samir\'s Typer Improver";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lblWord.ForeColor = System.Drawing.Color.White;
+            this.lblWord.Location = new System.Drawing.Point(328, 151);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(0, 36);
+            this.lblWord.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblResult.ForeColor = System.Drawing.Color.White;
+            this.lblResult.Location = new System.Drawing.Point(328, 108);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 25);
+            this.lblResult.TabIndex = 4;
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(319, 282);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(121, 64);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblTime.ForeColor = System.Drawing.Color.White;
+            this.lblTime.Location = new System.Drawing.Point(547, 91);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 29);
+            this.lblTime.TabIndex = 7;
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDone.ForeColor = System.Drawing.Color.White;
+            this.btnDone.Location = new System.Drawing.Point(513, 282);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(149, 64);
+            this.btnDone.TabIndex = 8;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = false;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // timerResult
+            // 
+            this.timerResult.Tick += new System.EventHandler(this.timerResult_Tick);
+            // 
+            // btnStore
+            // 
+            this.btnStore.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStore.ForeColor = System.Drawing.Color.White;
+            this.btnStore.Location = new System.Drawing.Point(72, 282);
+            this.btnStore.Name = "btnStore";
+            this.btnStore.Size = new System.Drawing.Size(118, 66);
+            this.btnStore.TabIndex = 9;
+            this.btnStore.Text = "Store";
+            this.btnStore.UseVisualStyleBackColor = false;
+            this.btnStore.Click += new System.EventHandler(this.btnStore_Click);
+            // 
+            // btnFruits
+            // 
+            this.btnFruits.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnFruits.ForeColor = System.Drawing.Color.White;
+            this.btnFruits.Location = new System.Drawing.Point(196, 281);
+            this.btnFruits.Name = "btnFruits";
+            this.btnFruits.Size = new System.Drawing.Size(132, 64);
+            this.btnFruits.TabIndex = 10;
+            this.btnFruits.Text = "Fruits";
+            this.btnFruits.UseVisualStyleBackColor = false;
+            this.btnFruits.Click += new System.EventHandler(this.btnFruits_Click);
+            // 
+            // btnVegetables
+            // 
+            this.btnVegetables.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnVegetables.ForeColor = System.Drawing.Color.White;
+            this.btnVegetables.Location = new System.Drawing.Point(409, 282);
+            this.btnVegetables.Name = "btnVegetables";
+            this.btnVegetables.Size = new System.Drawing.Size(127, 64);
+            this.btnVegetables.TabIndex = 11;
+            this.btnVegetables.Text = "Vegetables";
+            this.btnVegetables.UseVisualStyleBackColor = false;
+            this.btnVegetables.Click += new System.EventHandler(this.btnVegetables_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblWarning.ForeColor = System.Drawing.Color.White;
+            this.lblWarning.Location = new System.Drawing.Point(12, 77);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(302, 25);
+            this.lblWarning.TabIndex = 12;
+            this.lblWarning.Text = "Capitilization and Spaces Count!!";
+            // 
+            // KeyboardTyper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.btnVegetables);
+            this.Controls.Add(this.btnFruits);
+            this.Controls.Add(this.btnStore);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtInput);
+            this.Name = "KeyboardTyper";
+            this.Text = "Samir\'s Typer Improver ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Timer timerResult;
+        private System.Windows.Forms.Button btnStore;
+        private System.Windows.Forms.Button btnFruits;
+        private System.Windows.Forms.Button btnVegetables;
+        private System.Windows.Forms.Label lblWarning;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Name: Samir Ali
+//Date: June 11, 2020
+//Assignment: Final Assessment (CPT)
+//Purpose: To improve the user's typing skills with multiple words that must be inputted.
+namespace KeyboardTypeRacer
+{
+    public partial class KeyboardTyper : Form
+    {
+        public static int correct = 0;
+        public static string[] fruits = new string[] {"Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blackcurrant", "Blueberry", "Boysenberry", "Cherry", "Coconut", "Fig", "Grape", "Grapefruit", "Kiwifruit", "Lemon", "Lime", "Lychee",
+                                                      "Mandarin", "Mango","Melon", "Nectarine","Orange", "Papaya","Passionfruit", "Peach","Pear", "Pineapple","Plum", "Pomegranate","Quince", "Raspberry","Strawberry", "Watermelon"}; //string array containing all words that are fruits
+        public static string[] vegetables = new string[] {"Asparagus", "Avocado", "Bean", "Beetroot", "Bok choy", "Broccoli", "Brussels sprouts", "Cabbage", "Capsicum", "Carrot", "Cauliflower", "Celery", "Corn", "Cucumber", "Eggplant", "Lettuce", "Mushroom" //string array containing all words that are vegetables
+                                                          , "Onion", "Parsnip", "Peas", "Potato", "Pumpkin", "Radish", "Spinach", "Squash", "Turnip", "Zucchini "};
+        public static Random random = new Random();
+        public static int wordfruit;
+        public static int wordveg;
+        public static double time;
+        public static double Wrong;
+        public static double Right;
+        public static double Attempt;
+        public static double accuracy;
+        public static double wpm;
+        public static DateTime date = DateTime.Now; //assigns the variable date the date and time that the program is launched
+        public static DirectoryInfo Result; //creates the variable Result with the data type DirectoryInfo to indicate the name of the directory
+        public static int determine;
+        public static List<int> listwordsfruit = new List<int>(); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+        public static List<int> listwordsvegetables = new List<int>(); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+        public KeyboardTyper()
+        {
+            InitializeComponent();
+            txtInput.Visible = false; //makes the textbox and other controls invisible
+            lblWord.Visible = false;
+            btnNext.Visible = false;
+            wordfruit = random.Next(0, 33); // randomizes the variable wordfruit between a number from 0-33
+            listwordsfruit.Add(wordfruit);
+            wordveg = random.Next(0, 23); // randomizes the variable wordveg between a number from 0-23
+            listwordsvegetables.Add(wordveg);
+            btnDone.Visible = false; 
+            btnStore.Visible = false;
+            lblWarning.Visible = false;
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) //checks to see if the key enter is pressed
+            {
+                btnNext.PerformClick(); //a click on the next button is mimicked when the enter key is pressed
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            btnDone.Visible = true; //the done button becomes visible when the next button is clicked
+            if (determine == 1) //if button fruits is clicked then the steps for fruits will be executed
+            {
+
+
+                if (txtInput.Text == fruits[wordfruit]) //If the word's spelling matches the user's input
+                {
+                    lblResult.Text = "Keep it up!";
+                    if (listwordsfruit.Count <= 32)
+                    {
+                        do
+                        {
+                            wordfruit = random.Next(0, 33); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                        }
+                        while (listwordsfruit.Contains(wordfruit));
+                    } //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                    else if (listwordsfruit.Count >= 33)
+                    {
+                        wordfruit = random.Next(0, 33);
+                    }
+                    listwordsfruit.Add(wordfruit); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                    lblWord.Text = fruits[wordfruit]; //the label displays the new word
+                    txtInput.Clear(); //the text box is cleared for the next word
+                    Attempt += 1; //One is added to the attempt variable every time a word is attempted
+                    Right += 1; //The variable right is added by 1 if the word matches user's input
+                }
+                else
+                {
+                    lblResult.Text = "Try Again!"; //letting the user know to try again
+                    txtInput.Clear();
+                    Wrong += 1; //adding one to the wrong variable if the answer is wrong
+                    Attempt += 1;
+                }
+            }
+            else if(determine == 2) //if the vegetables button is clicked then the following block of code will run
+            {
+                if (txtInput.Text == vegetables[wordveg])
+                {
+                    lblResult.Text = "Keep it up!";
+                    if (listwordsvegetables.Count <= 21)
+                    {
+                        do
+                        {
+                            wordveg = random.Next(0, 23); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                        }
+                        while (listwordsvegetables.Contains(wordveg));
+                    } //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                    else if (listwordsvegetables.Count >= 22)
+                    {
+                        wordveg = random.Next(0, 23);
+                    }
+                        listwordsvegetables.Add(wordveg); //sourced from https://stackoverflow.com/questions/30014901/generating-random-numbers-without-repeating-c
+                        lblWord.Text = vegetables[wordveg];
+                        txtInput.Clear();
+                        Attempt += 1;
+                        Right += 1;
+                    
+                }
+                else
+                {
+                    lblResult.Text = "Try Again!";
+                    txtInput.Clear();
+                    Wrong += 1;
+                    Attempt += 1;
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time += 1; //the variable time is added by 1 every second
+        }
+
+        private void btnDone_Click(object sender, EventArgs e) 
+        {
+            accuracy = Right / Attempt * 100; //the accuracy percentage is calculated by dividing the right and the attempts times 100
+            accuracy = Math.Round(accuracy, 1); //the variable accuracy is rounded to one decimal place
+            double timeinMins = time / 60; //the variable timeinMins stores the value of the time elapsed in mins
+            wpm = Right / timeinMins; //the variable wpm stores the value of the words per minute by dividing right and timeinmins
+            wpm = Math.Round(wpm, 1);
+            lblResult.Text = "Accuracy: " + accuracy.ToString() + "%" + "\n WPM = " + wpm.ToString(); //the user's results are outputted
+            lblWord.Visible = false; //labels and other controls are hidden
+            timer1.Enabled = false;
+            btnStore.Visible = true;
+            lblWarning.Visible = false;
+        }
+
+        private void timerResult_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = " Time in (s): " + time.ToString() + "\n Attempt:" + Attempt + "\n Correct:" + Right + "\n Incorrect:" + Wrong; // a timer is created to constantly show the results while the user is using the program
+        }
+
+        private void btnStore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Result = Directory.CreateDirectory(@"C:\Users\computer\Downloads\Results"); //a folder is created with the name results
+                if (File.Exists(@"C:\Users\computer\Downloads\Results\Results.txt")) //checks to see if a file with the name results exist
+                {
+                    using (StreamWriter sw = File.AppendText(@"C:\Users\computer\Downloads\Results\Results.txt")) //if a file does exist it will write to the file instead of creating a new one
+                    {
+                        sw.WriteLine("\n" + date.ToString() + "\nAccuracy: " + accuracy.ToString() + "%" + "\nWPM: " + wpm.ToString() + "\nTime Taken: " + time + " Seconds"); //stores results with date and time
+                        MessageBox.Show("Check Results folder for results!");
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.CreateText(@"C:\Users\computer\Downloads\Results\Results.txt")) //if a file doesn't exist then one is created with the name results
+                    {
+                        sw.WriteLine("\n" + date.ToString() + "\nAccuracy: " + accuracy.ToString() + "%" + "\nWPM: " + wpm.ToString() + "\nTime Taken: " + time + " Seconds"); //stores results with date and time
+                        MessageBox.Show("Check downloads folder for results!");
+                    }
+                }
+
+                if (!Directory.Exists(@"C:\Users\computer\Downloads")) //checks to see if the path exists
+                {
+                    MessageBox.Show("Create downloads folder in path C:Users|computer|Downloads for storage"); //if the path doesn't exist, the user is asked to create one
+                }
+            }catch(Exception)
+            {
+                MessageBox.Show("Problem with storing file, Ensure Path C:|Users|computer|Downloads exists" );
+            }
+        }
+        private void btnFruits_Click(object sender, EventArgs e)
+        {
+            //SETUP for the program is done by making certain controls visible and invisible 
+            determine = 1;
+            txtInput.Visible = true;
+            btnFruits.Visible = false;
+            btnVegetables.Visible = false;
+            btnNext.Visible = true;
+            lblWord.Text = fruits[wordfruit];
+            lblWord.Visible = true;
+            timer1.Enabled = true;
+            timerResult.Enabled = true;
+            BackgroundImage = Properties.Resources._500_F_78123835_NYa1r7I271AQWK8a5vncxlVxDcnaP0rc; //an image is loaded from resouces to become the background
+            lblWarning.Visible = true;
+        }
+
+        private void btnVegetables_Click(object sender, EventArgs e)
+        {
+            //SETUP for the program is done by making certain controls visible and invisible 
+            determine = 2;
+            txtInput.Visible = true;
+            btnFruits.Visible = false;
+            btnVegetables.Visible = false;
+            btnNext.Visible = true;
+            lblWord.Text = vegetables[wordveg];
+            lblWord.Visible = true;
+            timer1.Enabled = true;
+            timerResult.Enabled = true;
+            BackgroundImage = Properties.Resources.images__1_;
+            lblWarning.Visible = true;
+        }
+    }
+
+}
